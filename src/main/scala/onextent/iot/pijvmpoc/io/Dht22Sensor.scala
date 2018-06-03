6c01eff561cd8285bece88d28b2c9f914db0e7d6
Ed Sweeney <ed@onextent.com>
2018-06-02 21:15:16 -0700

    jni

--- src/main/scala/onextent/iot/pijvmpoc/io/Dht22Sensor.scala
+++ src/main/scala/onextent/iot/pijvmpoc/io/Dht22Sensor.scala
@@ -1,28 +1,24 @@
 package onextent.iot.pijvmpoc.io
 
-import io.circe
 import io.circe.generic.auto._
-import io.circe.parser._
+import onextent.iot.dht22.Dht22
 import onextent.iot.pijvmpoc.models.TempReading
 
 object Dht22Sensor {
 
   def apply(pin: Int): TempReading = {
 
-    //val reading = Sensor.read(22, pin)
-    //val json = JSON.stringify(reading)
+    val dht22 = new Dht22
 
-    val json =
-      """{"temperature": 25.1, "humidity": 99.9, "isValid": true, "errors": 0}"""
+    val reading = dht22.get(4)
 
-    val decoded: Either[circe.Error, TempReading] = decode[TempReading](json)
-
-    // todo: keep the Either for error reporting via IOT but hide the circe type
-    decoded match {
-      case Left(e) =>
-        println(s"no reading: $e")
-        TempReading()
-      case Right(r) => r
+    if (reading(0) > 0) {
+      TempReading(Some(reading(0).toDouble),
+                  Some(reading(2).toDouble),
+                  None,
+                  None)
+    } else {
+      TempReading()
     }
 
   }
